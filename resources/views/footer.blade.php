03bd169d195fff64d40b644a7c053ea8a91cf5fd
ikhlas <ikhlaskhan973@gmail.com>
2021-06-14 11:05:04 -0800

    Initial commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,11 @@
+<div class="card text-center">
+  
+  <div class="card-body">
+    <h5 class="card-title">Buy and Sell</h5>
+    <p class="card-text">Grow you business with Trust.</p>
+    
+  </div>
+  <div class="card-footer text-muted">
+    We are here to facilitate you
+  </div>
+</div>
---
+++ resources/views/master.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+
+<!-- JQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <title>Ecommerce</title>
+</head>
+<body>
+     {{View::make('header')}}
+     @yield('content')
+     {{View::make('footer')}}
+</body>
+
+ <style>
+    .custom-login{
+
+        height: 500px;
+        padding: 50px 30px;
+    }
+ </style>
+</html>
